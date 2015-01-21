b7eea3d0d1bded9f8423825a0d7fe96aa538976f
Vishnu <Vishnu@gmail.com>
2015-01-21 14:46:34 +0530

    added cpntroller test

--- src/test/java/com/sample/mvc/controllerTest/EmployeeControllerTest.java
+++ src/test/java/com/sample/mvc/controllerTest/EmployeeControllerTest.java
@@ -3,82 +3,125 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.sample.mvc.controller.EmployeeController;
 import com.sample.mvc.model.EmployeeCommand;
 import com.sample.mvc.service.EmployeeService;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-//@ContextConfiguration(classes=BaseTestConfig.class)
-@ContextConfiguration(locations = {"classpath*:/WEB-INF/employee-servlet.xml"})
 
-//@Transactional
+@RunWith(MockitoJUnitRunner.class)
 public class EmployeeControllerTest {
 	
-//	@Autowired
-//	private WebApplicationContext wac;
+
 	
 	
-	 @Mock
-	 private EmployeeService employeeService;
-	    @InjectMocks
-	    private EmployeeController employeeController;
+	 public EmployeeService employeeService=mock(EmployeeService.class);
 	 
+	 
+	    @InjectMocks
+	    public EmployeeController employeeController=new EmployeeController();
+	    EmployeeCommand emp;
 	    private MockMvc mockMvc;
 	 
 	    @Before
 	    public void setup() {
 	 
-	        // Process mock annotations
 	        MockitoAnnotations.initMocks(this);
 	 
-	        // Setup Spring test in standalone mode
 	        this.mockMvc = MockMvcBuilders.standaloneSetup(employeeController).build();
-}
+	        emp = new EmployeeCommand();
+	    	 emp.setCode(105);
+	    	 emp.setName("vikas");
+	    	 emp.setCity("pune");
+	        }
+	    
+	        
+
 	    
 	    @Test
-	    public void testAddEmployee() throws Exception {
-	     
-	    	     
-	        this.mockMvc.perform(post("/add")
-	                .param("code", "101")
-	                .param("name", "mvcfirst")
-	                .param("city", "mvclastname"))
-	                .andExpect(view().name("redirect:list"))
-	                .andExpect(model().attributeExists("employeeCommand"));
+	    public void testAddEmployee() throws Exception 
+	    {
+	    	
+	    	ModelAndView modelandview=mock(ModelAndView.class);
+	        Mockito.doNothing().when(employeeService).addEmployee(any(EmployeeCommand.class));
+	        assertNotNull(employeeController.addEmployee(emp));
+	        assertFalse(employeeController.addEmployee(emp).isEmpty());
+	        
 	     
 	    }
 	    
 	    @Test
-		public void findById()
-				throws Exception {
-	    	
-	    	 EmployeeCommand emp = new EmployeeCommand();
-	    	 emp.setCode(105);
-            emp.setName("vikas");
-            emp.setCity("pune");
+		public void testGetEmployeeById()   throws Exception 
+		{
+	    	Model model=mock(Model.class);
+			when(employeeService.getEmployeebyId(anyInt())).thenReturn(emp);
+			assertEquals("vikas",employeeService.getEmployeebyId(anyInt()).getName());
+			model.addAttribute("employee", emp);
+			assertEquals("edit",employeeController.getEmployeeById(20, model));
+		}
+	    
+	    
+	    @Test
+		public void testViewHome()
+		{
 			
-
-			this.mockMvc
-					.perform(post("/edit/{code}", 105))
-					.andExpect(view().name("edit"))
-					.andExpect(forwardedUrl("edit"))
-//					.andExpect(model().attribute("employee",  105 /*hasProperty("code", is(105))*/))
-					;
-
+			Assert.assertEquals("home", employeeController.viewHome());
 			
 		}
+	    
+	    @Test
+	    public void testUpdateEmployee() throws Exception
+	    {
+           doNothing().when(employeeService).updateEmployee(any(EmployeeCommand.class));
+           assertEquals("redirect:/list",employeeController.updateEmployee(emp));
+	    	
+	    }
+	    
+	    
+	    @Test
+	    public void testDeleteEmployee() throws Exception
+	    {
+	    	doNothing().when(employeeService).deleteEmployee(anyInt());
+	    	assertEquals("redirect:/list",employeeController.deleteEmplyee(101));
+	    }
+	    
+	    
+	    @Test
+	    public void testListContacts() throws Exception
+	    {
+          List<EmployeeCommand> empList=mock(List.class);
+          empList.add(emp);
+          when(employeeService.listContact()).thenReturn(empList);
+          Map<String,Object> map=mock(Map.class);
+          map.put("contact", new EmployeeCommand());
+          map.put("contactList",employeeService.listContact() );
+          when(map.size()).thenReturn(2);
+          assertEquals(2,map.size());
+          assertEquals("list",employeeController.listContacts(map));
+	    }
+	    
 }
