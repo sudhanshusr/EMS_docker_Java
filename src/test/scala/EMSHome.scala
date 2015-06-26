76cabd02e1f318e21d32a4151ff8fc42091af9bd
Vijeta Angeer <vijetaa@cybage.com>
2015-06-26 16:55:08 +0530

    changed gatling plugin version in pom.xml

--- src/test/scala/EMSHome.scala
+++ src/test/scala/EMSHome.scala
@@ -1,34 +1,42 @@
+package computerdatabase
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
 class EMSHome extends Simulation {
+  // your code starts here
 
   val scn = scenario("EMS Scenario")
     .exec(http("Home Page")
-      .get("http://172.27.59.18:8085/EmployeeApplication/home.htm"))
+      .get("http://172.27.59.40:8080/EmployeeApplication/home.htm"))
     .pause(10)
     .exec(http("request_add")
-      .post("http://172.27.59.18:8085/EmployeeApplication/jsp/add.htm")
-      .param("""code""", """1""")
-      .param("""name""", """a""")
-      .param("""city""", """b"""))
+      .post("http://172.27.59.40:8080/EmployeeApplication/add.htm")
+      .formParam("""code""", """1""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
+      .formParam("""name""", """a""")
+      .formParam("""city""", """b"""))
     .pause(1)
     .exec(http("request_edit")
-      .get("http://172.27.59.18:8085/EmployeeApplication/edit.jsp?code=1&name=a&city=a"))
+      .get("http://172.27.59.40:8080/EmployeeApplication/edit.jsp?code=1&name=a&city=a"))
     .pause(1)
     .exec(http("request_edit submit")
-      .post("http://172.27.59.18:8085/EmployeeApplication/edit.htm")
-      .param("""code""", """1""")
-      .param("""name""", """b""")
-      .param("""city""", """c"""))
+      .post("http://172.27.59.40:8080/EmployeeApplication/edit.htm")
+      .formParam("""code""", """1""")
+      .formParam("""name""", """b""")
+      .formParam("""city""", """c"""))
     .pause(1)
     .exec(http("request_delete")
-      .get("http://172.27.59.18:8085/EmployeeApplication/delete.jsp?code=1"))
+      .get("http://172.27.59.40:8080/EmployeeApplication/delete.jsp?code=1"))
     .pause(10)
     .exec(http("request_delete submit")
-      .post("http://172.27.59.18:8085/EmployeeApplication/delete.htm")
-      .param("""code""", """1"""))
+      .post("http://172.27.59.40:8080/EmployeeApplication/delete.htm")
+      .formParam("""code""", """1"""))
 
-  setUp(scn.inject(ramp(10 users) over (2 seconds)))
+      setUp(scn.inject(rampUsers(100) over (10 seconds))).assertions(
+        global.successfulRequests.percent.greaterThan(90)
+    )
+  //setUp(scn.inject(rampUsers(100) over (10 seconds)))
+  //  setUp(scn.inject(atOnceUsers(10)))
+  // your code ends here
 }
